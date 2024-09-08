61e9853a2cc92c27928de88fb05a6d3955519ee1
Falkono <yoda0108@gmail.com>
2024-09-08 22:30:37 +0200

    rotation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -8,6 +9,11 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +21,11 @@ def main():
                 return
         
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
